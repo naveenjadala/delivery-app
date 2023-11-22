76566fba3063658d04e0ff5eb4a069cd3765b144
Naveen Jadala <naveen.jadala@gmail.com>
2023-11-22 14:33:49 +0100

    chore: launcher image updates

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,19 +1,16 @@
-import {configureStore} from '@reduxjs/toolkit';
-import { combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import restaurantsSlice from './slices/restaurantsSlice';
-
 
 const preloadedState = {};
 
 const reducer = combineReducers({
-    restaurants: restaurantsSlice
-
-})
+  restaurants: restaurantsSlice,
+});
 
 export const store = configureStore({
   preloadedState,
   reducer,
-  //   devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
